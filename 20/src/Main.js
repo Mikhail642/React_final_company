c567ace7458c4023692ed71d652221682511525f
Mikhail642 <63279288+Mikhail642@users.noreply.github.com>
2020-05-31 22:50:53 +0900

    Update Main.js

--- 20/src/Main.js
+++ 20/src/Main.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import {Route, NavLink, HashRouter} from 'react-router-dom'
 import home from './Home';
 import ocompanyy from './ocompanyy';
-import './Cssmodule.css';
+import  s from './index.module.css';
 
 
 class Main extends Component {
     render() {
         return (
             <HashRouter>
-               <div className="Css.module.div">
+                <div className={s.div}> 
                     <h1>Финальный проект на реакт</h1>
                     <ul className="header">
     <li><NavLink exact to="/home">Главная</NavLink></li>

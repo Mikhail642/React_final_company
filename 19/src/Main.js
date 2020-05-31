cf1db282b8a11d824a5abcb921615960b2c05349
Mikhail <mixail.schherbakov.9898@mail.ru>
2020-05-31 20:10:29 +0900

    19
    19

---
+++ 19/src/Main.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Route, NavLink, HashRouter} from 'react-router-dom'
+import home from './Home';
+import ocompanyy from './ocompanyy';
+import './index.css';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Финальный проект на реакт</h1>
+                    <ul className="header">
+    <li><NavLink exact to="/home">Главная</NavLink></li>
+    <li><NavLink to="/ocompanyy">О компании</NavLink></li>
+</ul>
+                    <div className="content">
+    <Route exact path="/home" component={home} />
+    <Route path="/ocompanyy" component={ocompanyy} />
+</div>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Main;
